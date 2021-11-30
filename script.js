71069331fafab414306e88556aa2fe00291d4746
Rif-7 <muhammadrifan974530@gmail.com>
2021-11-30 19:51:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+function playSound(e) {
+    let code = e.target.getAttribute("data-keyNo");
+    console.log(code);
+    const audio = document.querySelector(`audio[data-key="${code}"]`);
+    e.target.classList.add("playing");
+}
+
+  const keys = Array.from(document.querySelectorAll('.image'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  keys.forEach(key => key.addEventListener("click", playSound));
